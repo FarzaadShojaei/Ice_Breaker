5bbb653719776589d6f80f9b6825397bd42c7d4a
farzaadshojaei <farzaadshojaei@gmail.com>
2024-05-05 21:57:41 +0330

    Adding OPEN AI Package

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,12 +1,24 @@
 from dotenv import load_dotenv
 from langchain.chains import LLMChain
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Hello Langchain")
+    information= """
+    Ever wondered about your role in #ImmersiveExperiences?
+
+    In #Dubai,
+    visitors personalize animals,
+    enriching a communal jungle.
+    While #AI aids walking models, the essence lies in visitor interaction,
+    Explore Dibulo app for at-home engagement. Reminiscent of teamLab Inc.'s Sketch Aquarium at The Newark Museum of Art of Art, 
+    where drawn sea creatures swam in a virtual aquarium. Immersion goes beyond observation; 
+    it's about active participation shaping our #DigitalLandscapes. How can your creativity transform the spaces you encounter? Join the conversation.
+
+    """
     print(os.environ['OPENAI_API_KEY'])
 
     summary_template="""
@@ -20,5 +32,10 @@ if __name__ == '__main__':
     summary_prompt_template=PromptTemplate(input_variables=["information"],template=summary_template)
 
     llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    
+    chain = LLMChain(llm=llm , prompt=summary_prompt_template)
+    res = chain.invoke(input={"information":information})
+
+    print(res)
 
 
